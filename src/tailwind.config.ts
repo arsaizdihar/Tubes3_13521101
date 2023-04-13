8e9388b9dd43095efe7d01bdf3399e64c1fd3598
arsaizdihar <arsadihar@gmail.com>
2023-04-13 20:52:13 +0700

    Create navigation UI

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -2,8 +2,16 @@ import { type Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          "800": "#343541",
+          "900": "#202123",
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
