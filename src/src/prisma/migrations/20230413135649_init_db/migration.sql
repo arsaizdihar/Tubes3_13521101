67002e13821f646ec9ab01d3f263cf3cee97fff0
arsaizdihar <arsadihar@gmail.com>
2023-04-30 21:53:04 +0700

    fix: remove additional prisma

--- src/src/prisma/migrations/20230413135649_init_db/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "ChatStorage" (
-    "id" SERIAL NOT NULL,
-    "question" TEXT NOT NULL,
-    "answer" TEXT NOT NULL,
-
-    CONSTRAINT "ChatStorage_pkey" PRIMARY KEY ("id")
-);
--- src/src/prisma/migrations/add_question/migration.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "Question" (
-    "id" SERIAL NOT NULL,
-    "question" TEXT NOT NULL,
-    "answer" TEXT NOT NULL
-);
