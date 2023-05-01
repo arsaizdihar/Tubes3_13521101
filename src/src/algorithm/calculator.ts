d465506ddd96544f5ac28c7ca13e288c20d520d3
vanessrw <64979714+vanessrw@users.noreply.github.com>
2023-05-01 23:19:20 +0700

    fix: eval -> stack

--- src/src/algorithm/calculator.ts
+++ src/src/algorithm/calculator.ts
@@ -2,6 +2,7 @@ import BaseAlgorithm from "./base";
 
 export class Calculator implements BaseAlgorithm {
   private regex = /^[\d+\-*/^()\s]+(\?)?$/;
+
   constructor() {}
 
   getResponse(input: string) {
@@ -12,8 +13,7 @@ export class Calculator implements BaseAlgorithm {
     }
 
     try {
-      // Evaluate the expression using eval()
-      const result = eval(expression.replace(/\^/g, "**")); // Replace ^ with ** to use it as power operator
+      const result = this.evaluate(expression);
       return "Hasilnya adalah " + result.toString();
     } catch (error) {
       return "Sintaks persamaan tidak sesuai";
@@ -22,6 +22,82 @@ export class Calculator implements BaseAlgorithm {
 
   isMatch(input: string) {
     return this.regex.test(input);
+  }
+
+  private evaluate(expression: string) {
+    const tokens = expression.match(/(\d+)|([\+\-\*\/\^\(\)])/g) || [];
+    const opStack: string[] = [];
+    const valStack: number[] = [];
+
+    for (const token of tokens) {
+      if (/^\d+$/.test(token)) {
+        valStack.push(parseInt(token, 10));
+      } else if (token === '(') {
+        opStack.push(token);
+      } else if (token === ')') {
+        while (opStack[opStack.length - 1] !== '(') {
+          const op = opStack.pop();
+          const b = valStack.pop() as number;
+          const a = valStack.pop() as number;
+          const result = this.operation(a, b, op as string);
+          valStack.push(result);
+        }
+        opStack.pop();
+      } else {
+        while (opStack.length > 0 && this.getPrecedence(opStack[opStack.length - 1]) >= this.getPrecedence(token)) {
+          const op = opStack.pop();
+          const b = valStack.pop() as number;
+          const a = valStack.pop() as number;
+          const result = this.operation(a, b, op as string);
+          valStack.push(result);
+        }
+        opStack.push(token);
+      }
+    }
+
+    while (opStack.length > 0) {
+      const op = opStack.pop();
+      const b = valStack.pop() as number;
+      const a = valStack.pop() as number;
+      const result = this.operation(a, b, op as string);
+      valStack.push(result);
+    }
+    return valStack[0];
+  }
+
+  private getPrecedence(operator: string) {
+    switch (operator) {
+      case '^':
+        return 3;
+      case '*':
+      case '/':
+        return 2;
+      case '+':
+      case '-':
+        return 1;
+      default:
+        return 0;
+    }
+  }
+
+  private operation(a: number, b: number, operator: string) {
+    switch (operator) {
+      case '^':
+        return Math.pow(a, b);
+      case '*':
+        return a * b;
+      case '/':
+        if (b === 0) {
+          throw new Error("Divide by zero error");
+        }
+        return a / b;
+      case '+':
+        return a + b;
+      case '-':
+        return a - b;
+      default:
+        throw new Error(`Invalid operator: ${operator}`);
+    }
   }
 }
 
